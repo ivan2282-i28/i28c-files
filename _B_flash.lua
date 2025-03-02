f5b41290f62f0f8a189afa61408a6cb719bf2611
ivan2282 <90458162+ivan2282-i28@users.noreply.github.com>
2025-03-02 18:09:39 +0300

    Update _B_flash.lua

--- _B_flash.lua
+++ _B_flash.lua
@@ -1,1 +1,5 @@
+print("AC bypass: start")
+game.ReplicatedStorage._BULTIN_DEBUG_ANTI_CHEAT:Fire("Back_0","Gj_","_main_","dbg","2x346")
+game.ReplicatedStorage._BULTIN_DEBUG_ANTI_CHEAT:Fire("Back_0","Gsssssssssssssssssssj_","_prt_","dbg","2x006")
+game.ReplicatedStorage._BULTIN_DEBUG_ANTI_CHEAT:Fire("Back_1","_rbx","_main_","dbg","2x078")
 print("AC bypassed")
